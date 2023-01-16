0f1bb352672ef406d7ae6919817695c964287770
Henry Hart <hart.h@etoncollege.org.uk>
2023-01-16 19:25:28 +0000

    Initial Commit

---
+++ CarCustomizer/CarCustomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomizerApp.swift
+//  CarCustomizer
+//
+//  Created by Hart, Henry (AGDF) on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
